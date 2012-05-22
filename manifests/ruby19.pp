ca40ed33ef624360c928c83268fafe953f4a7ae5
Nan Liu <nan@puppetlabs.com>
2012-05-22 05:43:18 -0500

    Fix missing gem and gem version issues.
    * Add additional gems required by razor.
    * Install macaddr v1.5.0 gem to avoid out of date systemu gem.

--- manifests/ruby19.pp
+++ manifests/ruby19.pp
@@ -29,9 +29,24 @@ class razor::ruby19 {
     }
   }
 
-  package { [ 'mongo', 'bson', 'bson_ext', 'rspec', 'syntax', 'uuid',
-              'logger', 'extlib', 'json', 'colored', 'bluepill', 'autotest',
-              'redcarpet', 'mocha', 'base62' ]:
+  # Need to specify macaddr 1.5.0 since 1.6.0 went backwards in dependencies.
+  package { 'macaddr':
+    ensure   => '1.5.0',
+    provider => 'gem',
+    require  => Exec['ruby_1.9.3_default'],
+  }
+
+  # Deploy uuid after macaddr so we don't pick up 1.6.0.
+  package { 'uuid':
+    ensure   => present,
+    provider => 'gem',
+    require  => Package['macaddr'],
+  }
+
+  package { [ 'autotest', 'base62', 'bluepill', 'bson', 'bson_ext', 
+              'colored', 'extlib', 'json', 'logger', 'mocha', 'mongo',
+              'net-ssh', 'redcarpet', 'require_all', 'rspec', 'syntax'
+            ]:
     ensure   => present,
     provider => gem,
     require  => Exec['ruby_1.9.3_default'],
