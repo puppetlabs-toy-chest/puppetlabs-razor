ad8f4ba53e2d6fc27e53d407c6088dad7d47b54c
Daniel Pittman <daniel@rimspace.net>
2012-10-23 16:23:56 -0700

    Restructure option handling to be more script-like
    Drop development-only gems from the module
    
    The `autotest` and `mocha` gems are only required for testing, not for running
    Razor in production.  We shouldn't install them from this module.
    
    Signed-off-by: Daniel Pittman <daniel@rimspace.net>

--- spec/classes/razor_ruby_spec.rb
+++ spec/classes/razor_ruby_spec.rb
@@ -12,8 +12,8 @@ describe 'razor::ruby', :type => :class do
         should include_class('ruby')
       }
       it {
-        [ 'autotest', 'base62', 'bson', 'bson_ext', 'colored',
-          'daemons', 'json', 'logger', 'macaddr', 'mocha', 'mongo',
+        [ 'base62', 'bson', 'bson_ext', 'colored',
+          'daemons', 'json', 'logger', 'macaddr', 'mongo',
           'net-ssh', 'require_all', 'syntax', 'uuid'
         ].each do |pkg|
           should contain_package(pkg).with(
